a08c791dc92914d7299d153ec3dca09733baeabd
John Wallace <jdwallace12@gmail.com>
2013-06-14 19:42:43 -0400

    more style fixes and added show page

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,14 +1,14 @@
 class Farm < ActiveRecord::Base
-  attr_accessible :adress, :city, :contact_info, :farm_description, :name, :number_of_shares, :phone_number, :share_description, :zipcode
-  validates_presence_of :adress
+  attr_accessible :address, :city, :state, :farm_description, :name, :number_of_shares, :phone_number, :share_description, :share_price, :certification
+  validates_presence_of :address
   validates_presence_of :city
-  validates_presence_of :contact_info
+  validates_presence_of :share_price
   validates_presence_of :farm_description
   validates_presence_of :name
   validates_presence_of :number_of_shares
   validates_presence_of :share_description
-  validates_presence_of :zipcode
+  validates_presence_of :certification
+  validates_presence_of :state
   belongs_to :user
-  belongs_to :farm_ownership
   has_many :comments
 end
--- app/models/farm_ownership.rb
+++
@@ -1,3 +0,0 @@
-class FarmOwnership < ActiveRecord::Base
-  # attr_accessible :title, :body
-end
