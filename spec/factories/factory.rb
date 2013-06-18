34bf0454c722d509dcf98d71ffa98deb7d185b84
John Wallace <jdwallace12@gmail.com>
2013-06-18 16:04:36 -0400

    added some model tests

---
+++ spec/factories/factory.rb
@@ -0,0 +1,24 @@
+FactoryGirl.define do
+  factory :farm do
+    sequence(:name) { |n| "Wallace Farms #{n}" }
+    sequence(:farm_description) { |n| "Super organic farm #{n}." }
+    sequence(:address) { |n| "12 #{n} Fake Street." }
+    sequence(:city) { |n| "Boston #{n}." }
+    sequence(:state) { |n| "Vermont #{n}." }
+    sequence(:number_of_shares) { |n| "1 #{n}." }
+    sequence(:share_price) { |n| "10 #{n}." }
+    sequence(:share_description) { |n| "big box of veggies #{n}." }
+
+  end
+
+  factory :user do
+    sequence(:email) { |n| "johnisthebest@wallace.com" }
+    password "12345678"
+    password_confirmation "12345678"
+    admin false
+    factory :admin do
+      admin true
+    end
+  end
+
+end
